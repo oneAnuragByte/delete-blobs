dbb90a7c420c36fece91662620a9bf7914ecbcf9
Anurag <70774983+oneAnuragByte@users.noreply.github.com>
2020-11-05 01:08:33 +0530

    Initial commit. Adds base script file and updates README file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # delete-blobs
 Delete large number of blobs in chunks of 10000 in Azure Automation
+
+This PowerShell script, designed to run in Azure Automatiom, deletes huge number of blobs in a container, by processing them in chunks of 10,000 blobs at a time. When the  number of blobs grows beyond a couple of thousands, the usual method of deleting each blob at a time may just get suspended without completing the task.
+
+This could be used to to delete all blobs (when parameter retentionDays is supplied as 0), or certain blobs which has not been modified for the last rententionDays number of days.
+
+
+## Note: 
+The script was written long back and has not been maintained/tested for a while. 
+It was erlier available here: https://gallery.technet.microsoft.com/Delete-large-number-of-97e04976
+
+Since Technet Gallery is retiring, I wanted to keep a backup here - in case we might need it later ;-)
---
+++ delete-blobs.ps1
@@ -0,0 +1,105 @@
+﻿<#
+.Synopsis
+  Deletes large number of blobs in a container of Storage account, which are older than x days
+
+.DESCRIPTION
+  This Runbook deletes huge number of blobs in a container, by processing them in chunks of 10,000 blobs at a time. When the number of blobs grow beyond a couple of thousands, the usual method of deleting each blob at a time may just get suspended without completing the task. 
+
+.PARAMETER CredentialAssetName
+	The Credential asset which contains the credential for connecting to subscription
+
+.PARAMETER Subscription
+	Name of the subscription attached to the credential in CredentialAssetName
+
+.PARAMETER container
+	Container name from which the blobs are to be deleted
+
+.PARAMETER AzStorageName
+	The Storage Name to which the container belong to
+	
+.PARAMETER retentionDays
+	Retention days. Blobs older than these many days will be deleted. To delete all, use 0
+	
+.NOTES
+   AUTHOR: Anurag Singh, MSFT
+   LASTEDIT: March 30, 2016
+#>
+
+function delete-blobs
+{   
+    param (
+        [Parameter(Mandatory=$true)] 
+        [String]  $CredentialAssetName,
+        
+        [Parameter(Mandatory=$true)]
+        [String] $Subscription,
+
+        [Parameter(Mandatory=$true)] 
+        [String] $container,
+		
+		[Parameter(Mandatory=$true)] 
+        [String] $AzStorageName,
+		
+		[Parameter(Mandatory=$true)] 
+        [Int] $retentionDays
+    )
+
+$Cred = Get-AutomationPSCredential -Name $CredentialAssetName
+$Account = Add-AzureAccount -Credential $Cred
+
+if(!$Account) 
+{
+    write-output "Connection to Azure Subscription using the Credential asset failed..."
+	Break;
+}
+
+set-AzureSubscription -SubscriptionName $Subscription
+
+$AzStorageKey = (Get-AzureStorageKey -StorageAccountName $AzStorageName).Primary
+$context = New-AzureStorageContext -StorageAccountName $AzStorageName -StorageAccountKey $AzStorageKey
+
+
+$blobsremoved = 0
+$MaxReturn = 10000
+$Total = 0
+$Token = $Null
+$TotalDel = 0
+$dateLimit = (get-date).AddDays(-$retentionDays) 
+
+try
+{
+	do
+	{
+		Write-Output "Retrieving blobs"
+		$blobs = Get-AzureStorageBlob -Container $container -context $context -MaxCount $MaxReturn  -ContinuationToken $Token 
+		$blobstodelete =  $blobs | where LastModified -LE $dateLimit
+		$Total += $Blobs.Count
+		Write-Output "$Total  total Retrieved blobs"
+ 
+		$Token = $Blobs[$blobs.Count -1].ContinuationToken;
+
+		if($Blobs.Length -le 0) 
+		{ 
+			break;
+		}
+
+		if($blobstodelete.Length -le 0) 
+		{ 
+			continue;
+		}
+
+		$TotalDel += $blobstodelete.Count
+
+		$blobstodelete  | Remove-AzureStorageBlob -Force 
+
+		Write-Output "$TotalDel  blobs deleted"
+	}
+	While ($Token -ne $Null)
+}
+
+catch 
+{
+	write-output $_
+}
+
+}
